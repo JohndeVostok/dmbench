e3a0f30b237b22c6c9dbcd779a3923832297a648
mzx <mazx15@mails.tsinghua.edu.cn>
2019-01-15 16:04:48 +0800

    refactor frontend

--- src/dmbench.py
+++ src/dmbench.py
@@ -1,69 +1,152 @@
 import wx
 import datetime
 import benchconn
+import time
+import threading
+import random
+import wx.lib.plot as plot
+
+
+class SettingFrame(wx.Frame):
+    def __init__(self, parent):
+        super(SettingFrame, self).__init__(parent, title="Settings", size=(480, 560))
+        self.font = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        self.panel = wx.Panel(self)
+        self.panel.SetFont(self.font)
+
+        self.labelAddr = wx.StaticText(self.panel, label="Database Address", pos=(40, 40), size=(380, 40))
+        self.textAddr = wx.TextCtrl(self.panel, value="127.0.0.1:5326/tpcc", pos=(40, 80), size=(380, 40))
+        self.textAddr.Enable(True)
+        self.labelUser = wx.StaticText(self.panel, label="User", pos=(40, 120), size=(180, 40))
+        self.labelPass = wx.StaticText(self.panel, label="Password", pos=(240, 120), size=(180, 40))
+        self.textUser = wx.TextCtrl(self.panel, value="SYSDBA", pos=(40, 160), size=(180, 40))
+        self.textPass = wx.TextCtrl(self.panel, value="SYSDBA", pos=(240, 160), size=(180, 40))
+        self.textUser.Enable(True)
+        self.textPass.Enable(True)
+        self.labelHouse = wx.StaticText(self.panel, label="WareHouse", pos=(40, 200), size=(180, 40))
+        self.labelWorker = wx.StaticText(self.panel, label="LoadWorker", pos=(240, 200), size=(180, 40))
+        self.textHouse = wx.TextCtrl(self.panel, value="10", pos=(40, 240), size=(180, 40))
+        self.textWorker = wx.TextCtrl(self.panel, value="4", pos=(240, 240), size=(180, 40))
+        self.textHouse.Enable(True)
+        self.textWorker.Enable(True)
+        self.labelTerminal = wx.StaticText(self.panel, label="Terminal", pos=(40, 280), size=(180, 40))
+        self.labelTime = wx.StaticText(self.panel, label="RunTime", pos=(240, 280), size=(180, 40))
+        self.textTerminal = wx.TextCtrl(self.panel, value="10", pos=(40, 320), size=(180, 40))
+        self.textTime = wx.TextCtrl(self.panel, value="10", pos=(240, 320), size=(180, 40))
+        self.textTerminal.Enable(True)
+        self.textTime.Enable(True)
+        self.butSave = wx.Button(self.panel, label="Save", pos=(100, 420), size=(120, 40))
+        self.butCancel = wx.Button(self.panel, label="Cancel", pos=(240, 420), size=(120, 40))
+        self.Bind(wx.EVT_BUTTON, self.actSave, self.butSave)
+        self.Bind(wx.EVT_BUTTON, self.actCancel, self.butCancel)
+
+    def actSave(self, event):
+        self.Destroy()
+
+    def actCancel(self, event):
+        self.Destroy()
 
 
 class MainFrame(wx.Frame):
     def __init__(self, parent, *args, **kwargs):
         super(MainFrame, self).__init__(parent, *args, **kwargs)
-        self.font = wx.Font(24, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
+        self.font = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         self.panel = wx.Panel(self)
         self.panel.SetFont(self.font)
 
-        self.labelAddr = wx.StaticText(self.panel, label="Database Address", pos=(40, 40), size=(400, 40))
-        self.textAddr = wx.TextCtrl(self.panel, value="127.0.0.1:5326/tpcc", pos=(40, 80), size=(400, 40))
-        self.textAddr.Enable(True)
+        self.plotter = plot.PlotCanvas(self.panel)
+        self.plotter.SetPosition(pt=(20, 20))
+        self.plotter.SetInitialSize(size=(450, 300))
+        self.plotter.SetBackgroundColour("white")
 
-        self.labelUser = wx.StaticText(self.panel, label="User", pos=(40, 120), size=(180, 40))
-        self.labelPass = wx.StaticText(self.panel, label="Password", pos=(260, 120), size=(180, 40))
-        self.textUser = wx.TextCtrl(self.panel, value="SYSDBA", pos=(40, 160), size=(180, 40))
-        self.textPass = wx.TextCtrl(self.panel, value="SYSDBA", pos=(260, 160), size=(180, 40))
-        self.textUser.Enable(True)
-        self.textPass.Enable(True)
+        self.textLog = wx.TextCtrl(self.panel, pos=(480, 20), size=(450, 440), style=wx.TE_MULTILINE)
+        self.butSet = wx.Button(self.panel, label="Set", pos=(40, 420), size=(120, 40))
+        self.butLoad = wx.Button(self.panel, label="Load", pos=(180, 420), size=(120, 40))
+        self.butRun = wx.Button(self.panel, label="Run", pos=(320, 420), size=(120, 40))
 
-        self.labelHouse = wx.StaticText(self.panel, label="WareHouse", pos=(40, 200), size=(180, 40))
-        self.labelWorker = wx.StaticText(self.panel, label="LoadWorker", pos=(260, 200), size=(180, 40))
-        self.textHouse = wx.TextCtrl(self.panel, value="10", pos=(40, 240), size=(180, 40))
-        self.textWorker = wx.TextCtrl(self.panel, value="4", pos=(260, 240), size=(180, 40))
-        self.textHouse.Enable(True)
-        self.textWorker.Enable(True)
+        self.Bind(wx.EVT_BUTTON, self.actSet, self.butSet)
+        self.Bind(wx.EVT_BUTTON, self.actLoad, self.butLoad)
+        self.Bind(wx.EVT_BUTTON, self.actRun, self.butRun)
 
-        self.labelTerminal = wx.StaticText(self.panel, label="Terminal", pos=(40, 280), size=(180, 40))
-        self.labelTime = wx.StaticText(self.panel, label="RunTime", pos=(260, 280), size=(180, 40))
-        self.textTerminal = wx.TextCtrl(self.panel, value="10", pos=(40, 320), size=(180, 40))
-        self.textTime = wx.TextCtrl(self.panel, value="10", pos=(260, 320), size=(180, 40))
-        self.textTerminal.Enable(True)
-        self.textTime.Enable(True)
+        self.TIMESTACK = 200
+        self.queVal = [0 for i in range(self.TIMESTACK)]
 
-        self.butLock = wx.Button(self.panel, label="Lock", pos=(40, 400), size=(120, 40))
-        self.butLoad = wx.Button(self.panel, label="Load", pos=(180, 400), size=(120, 40))
-        self.butRun = wx.Button(self.panel, label="Run", pos=(320, 400), size=(120, 40))
-        self.butLock.Enable(True)
-        self.butLoad.Enable(False)
-        self.butRun.Enable(False)
+    def addValue(self, val):
+        self.queVal.append(val)
+        del self.queVal[0]
+#        print(" ".join([str(i) for i in self.queVal]))
+        data = [[i + 1 - self.TIMESTACK, self.queVal[i]] for i in range(self.TIMESTACK)]
+        line = plot.PolyLine(data, colour='red', width=2)
+        gc = plot.PlotGraphics([line], 'Performance', 'Time', 'Tpm-C')
+        self.plotter.Draw(gc)
 
-        self.textLog = wx.TextCtrl(self.panel, pos=(480, 40), size=(420, 440), style=wx.TE_MULTILINE)
+    def setLock(self, flag):
+        self.butSet.Enable(flag)
+        self.butRun.Enable(flag)
+        self.butLoad.Enable(flag)
 
-        self.addr = "127.0.0.1:5326/tpcc"
-        self.user = "SYSDBA"
-        self.password = "SYSDBA"
-        self.house = 10
-        self.worker = 4
-        self.terminal = 10
-        self.time = 10
-        self.lockStatus = False
-        self.logInfo = []
+    def actSet(self, event):
+        self.textLog.SetValue("set")
 
-        self.Bind(wx.EVT_BUTTON, self.lock, self.butLock)
-        self.Bind(wx.EVT_BUTTON, self.loadData, self.butLoad)
-        self.Bind(wx.EVT_BUTTON, self.runBench, self.butRun)
+    def actLoad(self, event):
+        settingFrame = SettingFrame(self)
+        settingFrame.Show(True)
+        self.textLog.SetValue("load")
 
-    def log(self, strLog):
-        nowTime = datetime.datetime.now().strftime("%H:%M:%S")
-        self.logInfo.append(nowTime + ":" + strLog)
-        strTmp = "\r\n".join(self.logInfo)
-        self.textLog.SetValue(strTmp)
+    def actRun(self, event):
+        self.setLock(False)
+        for i in range(2 * self.TIMESTACK):
+            self.addValue(random.randint(1, 1000))
+            time.sleep(0.001)
+        self.textLog.SetValue("run")
+        self.setLock(True)
 
+#        self.labelAddr = wx.StaticText(self.panel, label="Database Address", pos=(40, 40), size=(400, 40))
+#        self.textAddr = wx.TextCtrl(self.panel, value="127.0.0.1:5326/tpcc", pos=(40, 80), size=(400, 40))
+#        self.textAddr.Enable(True)
+#        self.labelUser = wx.StaticText(self.panel, label="User", pos=(40, 120), size=(180, 40))
+#        self.labelPass = wx.StaticText(self.panel, label="Password", pos=(260, 120), size=(180, 40))
+#        self.textUser = wx.TextCtrl(self.panel, value="SYSDBA", pos=(40, 160), size=(180, 40))
+#        self.textPass = wx.TextCtrl(self.panel, value="SYSDBA", pos=(260, 160), size=(180, 40))
+#        self.textUser.Enable(True)
+#        self.textPass.Enable(True)
+#        self.labelHouse = wx.StaticText(self.panel, label="WareHouse", pos=(40, 200), size=(180, 40))
+#        self.labelWorker = wx.StaticText(self.panel, label="LoadWorker", pos=(260, 200), size=(180, 40))
+#        self.textHouse = wx.TextCtrl(self.panel, value="10", pos=(40, 240), size=(180, 40))
+#        self.textWorker = wx.TextCtrl(self.panel, value="4", pos=(260, 240), size=(180, 40))
+#        self.textHouse.Enable(True)
+#        self.textWorker.Enable(True)#
+#        self.labelTerminal = wx.StaticText(self.panel, label="Terminal", pos=(40, 280), size=(180, 40))
+#        self.labelTime = wx.StaticText(self.panel, label="RunTime", pos=(260, 280), size=(180, 40))
+#        self.textTerminal = wx.TextCtrl(self.panel, value="10", pos=(40, 320), size=(180, 40))
+#        self.textTime = wx.TextCtrl(self.panel, value="10", pos=(260, 320), size=(180, 40))
+#        self.textTerminal.Enable(True)
+#        self.textTime.Enable(True)
+#        self.butLock.Enable(True)
+#        self.butLoad.Enable(False)
+#        self.butRun.Enable(False)
+
+#        self.addr = "127.0.0.1:5326/tpcc"
+#        self.user = "SYSDBA"
+#        self.password = "SYSDBA"
+#        self.house = 10
+#        self.worker = 4
+#        self.terminal = 10
+#        self.time = 10
+#        self.lockStatus = False
+#        self.logInfo = []
+
+#        self.Bind(wx.EVT_BUTTON, self.lock, self.butLock)
+#        self.Bind(wx.EVT_BUTTON, self.loadData, self.butLoad)
+
+#    def log(self, strLog):
+#        nowTime = datetime.datetime.now().strftime("%H:%M:%S")
+#        self.logInfo.append(nowTime + ":" + strLog)
+#        strTmp = "\r\n".join(self.logInfo)
+#        self.textLog.SetValue(strTmp)
+
+
+'''
     def setLock(self, flag):
         self.lockStatus = not flag
         self.textAddr.Enable(flag)
@@ -93,7 +176,7 @@ class MainFrame(wx.Frame):
             self.worker = int(self.textWorker.GetValue())
             self.terminal = int(self.textTerminal.GetValue())
             self.time = int(self.textTime.GetValue())
-            props = benchconn.genprop(self.addr, self.user, self.password, self.house, self.worker, self.terminal, self.time)
+            props = self.genprop(self.addr, self.user, self.password, self.house, self.worker, self.terminal, self.time)
             self.log("tmp.props updated.")
             self.setLock(False)
             self.butLock.SetLabel("Unlock")
@@ -103,7 +186,7 @@ class MainFrame(wx.Frame):
         self.setLockBut(False)
         s = benchconn.load()
         self.log(s)
-	self.setLockBut(True)
+        self.setLockBut(True)
         self.log("load finish!")
 
     def runBench(self, event):
@@ -113,7 +196,7 @@ class MainFrame(wx.Frame):
         self.log(s)
         self.setLockBut(True)
         self.log("run finish!")
-
+'''
 
 class App(wx.App):
     def OnInit(self):
