284d56376199e18fe974016e76487a862032758f
mzx <mazx15@mails.tsinghua.edu.cn>
2019-01-16 10:05:19 +0800

    add setting

--- src/dmbench.py
+++ src/dmbench.py
@@ -1,9 +1,10 @@
 import wx
 import datetime
-import benchconn
 import time
 import threading
 import random
+import os
+import subprocess as sp
 import wx.lib.plot as plot
 
 
@@ -14,33 +15,37 @@ class SettingFrame(wx.Frame):
         self.panel = wx.Panel(self)
         self.panel.SetFont(self.font)
 
+        self.data = {}
+        with open("tmp.prop") as f:
+        	lines = f.readlines()
+        	for line in lines:
+        	    if line.find("=") != -1:
+            		tmp = line.strip().split("=")
+#            		print(line, tmp)
+            		self.data[tmp[0]] = tmp[1]
+
         self.labelAddr = wx.StaticText(self.panel, label="Database Address", pos=(40, 40), size=(380, 40))
-        self.textAddr = wx.TextCtrl(self.panel, value="127.0.0.1:5326/tpcc", pos=(40, 80), size=(380, 40))
-        self.textAddr.Enable(True)
+        self.textAddr = wx.TextCtrl(self.panel, value=self.data["conn"], pos=(40, 80), size=(380, 40))
         self.labelUser = wx.StaticText(self.panel, label="User", pos=(40, 120), size=(180, 40))
         self.labelPass = wx.StaticText(self.panel, label="Password", pos=(240, 120), size=(180, 40))
-        self.textUser = wx.TextCtrl(self.panel, value="SYSDBA", pos=(40, 160), size=(180, 40))
-        self.textPass = wx.TextCtrl(self.panel, value="SYSDBA", pos=(240, 160), size=(180, 40))
-        self.textUser.Enable(True)
-        self.textPass.Enable(True)
+        self.textUser = wx.TextCtrl(self.panel, value=self.data["user"], pos=(40, 160), size=(180, 40))
+        self.textPass = wx.TextCtrl(self.panel, value=self.data["password"], pos=(240, 160), size=(180, 40))
         self.labelHouse = wx.StaticText(self.panel, label="WareHouse", pos=(40, 200), size=(180, 40))
         self.labelWorker = wx.StaticText(self.panel, label="LoadWorker", pos=(240, 200), size=(180, 40))
-        self.textHouse = wx.TextCtrl(self.panel, value="10", pos=(40, 240), size=(180, 40))
-        self.textWorker = wx.TextCtrl(self.panel, value="4", pos=(240, 240), size=(180, 40))
-        self.textHouse.Enable(True)
-        self.textWorker.Enable(True)
+        self.textHouse = wx.TextCtrl(self.panel, value=self.data["warehouses"], pos=(40, 240), size=(180, 40))
+        self.textWorker = wx.TextCtrl(self.panel, value="loadWorkers", pos=(240, 240), size=(180, 40))
         self.labelTerminal = wx.StaticText(self.panel, label="Terminal", pos=(40, 280), size=(180, 40))
         self.labelTime = wx.StaticText(self.panel, label="RunTime", pos=(240, 280), size=(180, 40))
-        self.textTerminal = wx.TextCtrl(self.panel, value="10", pos=(40, 320), size=(180, 40))
-        self.textTime = wx.TextCtrl(self.panel, value="10", pos=(240, 320), size=(180, 40))
-        self.textTerminal.Enable(True)
-        self.textTime.Enable(True)
+        self.textTerminal = wx.TextCtrl(self.panel, value=self.data["terminals"], pos=(40, 320), size=(180, 40))
+        self.textTime = wx.TextCtrl(self.panel, value=self.data["runMins"], pos=(240, 320), size=(180, 40))
         self.butSave = wx.Button(self.panel, label="Save", pos=(100, 420), size=(120, 40))
         self.butCancel = wx.Button(self.panel, label="Cancel", pos=(240, 420), size=(120, 40))
         self.Bind(wx.EVT_BUTTON, self.actSave, self.butSave)
         self.Bind(wx.EVT_BUTTON, self.actCancel, self.butCancel)
 
     def actSave(self, event):
+        self.data["conn"] = self.textAddr.Value()
+        self.data["user"] = self.text
         self.Destroy()
 
     def actCancel(self, event):
@@ -53,6 +58,8 @@ class MainFrame(wx.Frame):
         self.font = wx.Font(16, wx.DEFAULT, wx.NORMAL, wx.NORMAL)
         self.panel = wx.Panel(self)
         self.panel.SetFont(self.font)
+
+        os.chdir("../run")
 
         self.plotter = plot.PlotCanvas(self.panel)
         self.plotter.SetPosition(pt=(20, 20))
@@ -86,11 +93,11 @@ class MainFrame(wx.Frame):
         self.butLoad.Enable(flag)
 
     def actSet(self, event):
+        settingFrame = SettingFrame(self)
+        settingFrame.Show(True)
         self.textLog.SetValue("set")
 
     def actLoad(self, event):
-        settingFrame = SettingFrame(self)
-        settingFrame.Show(True)
         self.textLog.SetValue("load")
 
     def actRun(self, event):
